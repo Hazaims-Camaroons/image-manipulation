c7efa4ff355200875814bf8c5d6f450ce88b6a3e
Brian C Adams <adams2bn@m630slcf891e7.slcb.uc.edu>
2014-07-29 16:50:08 -0400

    Get in sync with derived data being removed.

---
+++ Image Manipulator/ManipulaterViewController~.h
@@ -0,0 +1,19 @@
+//
+//  ManipulaterViewController.h
+//  Image Manipulator
+//
+//  Created by Cody A Wisniewski on 7/23/14.
+//  Copyright (c) 2014 Hazems-Camaroons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManipulaterViewController : UIViewController
+{
+    NSArray* types;
+}
+@property (weak, nonatomic) IBOutlet UIPickerView *ImageManipulatorPicker;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIImageView *originalImage;
+- (IBAction)Save:(UIBarButtonItem *)sender;
+@end
